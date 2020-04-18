73f6d9a3bfdb32f1a1159df16697df96806a4b79
Nazim Balayoglu <balayev@gmail.com>
2020-04-19 00:34:53 +0100

    UI changes, added new feature of listing all countries' stats as a grid and new tabbed design.

--- virus_tracker.js
+++ virus_tracker.js
@@ -15,8 +15,9 @@ class VirusTracker {
   }
 
   setFieldsDisplay(isWorldWide) {
+    $('#all_tab').hide();    
     $('#flag').css('display', isWorldWide ? 'none' : 'block');
-    $('#danger-rank-tr').css('display', isWorldWide ? 'none' : '');
+    $('#dangerRankDiv').css('display', isWorldWide ? 'none' : '');
   }
 
   convert(response, isWorldWide, countryCode) {
@@ -26,10 +27,10 @@ class VirusTracker {
       total_cases: data['total_cases'],
       total_recovered: data['total_recovered'],
       total_deaths: data['total_deaths'],
-      total_new_cases_today: data['total_new_cases_today'],
-      total_new_deaths_today: data['total_new_deaths_today'],
+      total_cases_today: data['total_new_cases_today'],
+      total_deaths_today: data['total_new_deaths_today'],
       total_active_cases: data['total_active_cases'],
-      total_serious_cases: data['total_serious_cases'],
+      total_critical_cases: data['total_serious_cases'],
       flag_url: isWorldWide ? '' : this.baseUrl + '/images/flags/' + countryCode + '.png',
       source_url: isWorldWide ? data['source']['url'] : data['info']['source'],
       source_name: this.name
